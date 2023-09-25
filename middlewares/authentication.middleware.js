d75683ac181f11ca430951d7912566a0fca1084c
mrsaurabhmaske <saurabh.r.maske@gmail.com>
2023-09-25 17:08:42 +0530

    Deploying backend to a public repo

---
+++ middlewares/authentication.middleware.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authentication = (req, res, next) => {
+  try {
+    const token = localStorage.getItem("token");
+    if (token) {
+      jwt.verify(token, process.env.SECRET_CODE, (err, decoded) => {
+        if (err) {
+          res.status(400).send({
+            msg: "Invalid User Token...Please login again to continue;...",
+          });
+        } else {
+          req.body.userId = decoded._id;
+          req.body.userEmail = decoded.email;
+          req.body.userName = decoded.name;
+          next();
+        }
+      });
+      res.end();
+    } else {
+      res.status(400).send({
+        msg: "Error while veryfying user token...Please login again to continue;...",
+      });
+    }
+  } catch (error) {}
+};
+module.exports = {
+  authentication,
+};
